29c839ab60aa14469f4766b16d6d3207424b9205
joshva peyyala <joshvapeyyala@gmail.com>
2025-07-20 02:33:19 +0530

    Added Facebook Oauth provider along with a .html file for data_deletion privary for facebook

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,12 +2,17 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import SequelizeAdapter from "@auth/sequelize-adapter";
 import sequelize from "../../database/config/db_connection"; 
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
     }),
   ],
   adapter: SequelizeAdapter(sequelize),
